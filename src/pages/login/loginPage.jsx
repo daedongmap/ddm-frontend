3c776c5ac0d2161b41e39a6dba69a14b36d9aa80
smile0902 <rladjstn@dgsw.hs.kr>
2024-05-22 12:25:46 +0900

    :poop:
    DAuth 연결중

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
 import * as L from "./loginStyle";
 import { useNavigate } from "react-router-dom";
+import useLogin from "../../hooks/auth/useLogin";
+import tokenStore from "../../lib/token/token";
 
 function LoginPage() {
   const navigate = useNavigate();
+  const { login, error } = useLogin();
   const [isModalOpen, setIsModalOpen] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  const handleLogin = async () => {
+    const accessToken = await login(username, password); // DAuth를 통한 로그인 시도
+    if (accessToken) {
+      tokenStore.saveToken(accessToken);
+      navigate("/home");
+    } else {
+      console.error("로그인 실패:", error);
+    }
+  };
+
   return (
     <>
       <L.loginBackground>
@@ -17,13 +33,20 @@ function LoginPage() {
           <LoginModal isOpen={isModalOpen} onClose={closeModal} />
           <L.loginIdBox>
             <L.loginIdLabel>아이디</L.loginIdLabel>
-            <L.loginIdInput type="text" placeholder="아이디를 입력해주세요." />
+            <L.loginIdInput
+              type="text"
+              placeholder="아이디를 입력해주세요."
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </L.loginIdBox>
           <L.loginPwBox>
             <L.loginPwLabel>비밀번호</L.loginPwLabel>
             <L.loginPwInput
               type="password"
               placeholder="비밀번호를 입력해주세요."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </L.loginPwBox>
 
@@ -31,9 +54,7 @@ function LoginPage() {
             <L.loginKeepCheckbox type="checkbox" />
             <L.loginKeepLabel>로그인 유지</L.loginKeepLabel>
           </L.loginKeepCheckboxWrapper>
-          <L.loginButton onClick={() => navigate("/home")}>
-            로그인
-          </L.loginButton>
+          <L.loginButton onClick={handleLogin}>로그인</L.loginButton>
         </L.loginContainer>
       </L.loginBackground>
     </>
