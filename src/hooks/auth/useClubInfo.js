5575651d1d6c8f59c71e9d9471d8244b45d7b44c
smile0902 <rladjstn@dgsw.hs.kr>
2024-07-01 15:05:30 +0900

    :memo:

--- src/hooks/auth/useClubInfo.js
+++ src/hooks/auth/useClubInfo.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import CONFIG from "../../config/config.json";
 
 const useClubInfo = () => {
-  const getClubInfo = async () => {
-    const { data } = await axios.get(`${CONFIG.SERVER}/api/club/`);
+  const getClubInfo = async (clubName) => {
+    const { data } = await axios.get(`${CONFIG.SERVER}/api/club/${clubName}`);
     return data;
   };
   return { getClubInfo };
